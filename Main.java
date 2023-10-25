d61858a279b326c7293c516543b02b3beded6a13
Ean010 <148790703+Ean010@users.noreply.github.com>
2023-10-25 11:10:48 +0000

    w3

--- Main.java
+++ Main.java
@@ -2,11 +2,30 @@ public class Main {
     
     public static void main(String[] args) {
         Student s = new Student();
-        s.name = "abc";
-        System.out.println ("Student name: " +s.name);
+        s.setName("abc");
+        System.out.println ("Student name: " +s.getName());
+
+        s.setAge(20);
+        System.out.println ("Student age: " +s.getAge());
     }
 }
 
 class Student{
     private String name;
+    private int age;
+
+    public String getName() {
+      return name;
+    }
+    public void setName(String newName){
+        name = newName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int newAge){
+        age = newAge;
+    }
 }
